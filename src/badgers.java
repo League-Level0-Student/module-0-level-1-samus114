3ec6c0b038f19f28037a9650220b24c79399c67d
emerson20 <league@WTS21.attlocal.net>
2017-10-31 18:15:34 -0700

    badgers, checkpoint1

---
+++ src/badgers.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+
+public class badgers {
+	public static void main(String[] args) {
+		String bad = "snake";
+		String mom = " badger";
+		String jav = "mushroom";
+		for (int p = 0; p < 4; p++) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print(mom);
+				if (i < 9) {
+					System.out.print(",");
+				} else {
+					System.out.println("");
+				}
+
+			}
+			for (int j = 0; j < 2; j++) {
+				System.out.print(jav);
+				if (j == 1) {
+					System.out.println(", a");
+				} else {
+					System.out.print(", ");
+				}
+			}
+		for (int q = 0; q < 3; q++) {
+			System.out.println(bad9);
+		}
+		}
+	}
+}
